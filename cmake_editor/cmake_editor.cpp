e4874f99cdb20f4af40009a6c43e8e2f42a4db88
Nicholas Gill <mythagel@gmail.com>
2013-03-19 22:03:02 +1100

    stream parser working. Tested on local cmake files.    8450 >>>SUCCESS

--- cmake_editor/cmake_editor.cpp
+++ cmake_editor/cmake_editor.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "listfile.h"
+#include "listparser.h"
 
 #include <string>
 #include <vector>
@@ -13,6 +14,8 @@
 #include <iterator>
 #include <iostream>
 #include <fstream>
+
+#include <sstream>
 
 
 bool diff(const std::string& s, const std::string& file)
@@ -24,7 +27,7 @@ bool diff(const std::string& s, const std::string& file)
 	return s != contents;
 }
 
-int main(int argc, char* argv[])
+int main2(int argc, char* argv[])
 {
 	std::ifstream ifs;
 	std::istream* is = &std::cin;
@@ -85,4 +88,139 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+int main(int argc, char* argv[])
+{
+	struct listp : public cmake2::listparser_t
+	{
+		std::ostringstream s;
+
+		virtual void whitespace(const char* c, const char* end)
+		{
+			s << std::string(c, end);
+		}
+		virtual void comment(const char* c, const char* end)
+		{
+			s << '#' << std::string(c, end);
+		}
+		virtual void begin_command(const char* c, const char* end)
+		{
+			s << std::string(c, end);
+		}
+		virtual void open_bracket()
+		{
+			s << '(';
+		}
+		virtual void close_bracket()
+		{
+			s << ')';
+		}
+		virtual void argument(const char* c, const char* end, bool quoted)
+		{
+			if(quoted)
+				s << '"' << std::string(c, end) << '"';
+			else
+				s << std::string(c, end);
+		}
+		virtual void end_command()
+		{
+			// ni
+		}
+
+		virtual ~listp() = default;
+	};
+
+	struct listq : public cmake2::listparser_t
+	{
+		std::ostringstream s;
+
+		virtual void whitespace(const char* c, const char* end)
+		{
+			s << std::string(c, end);
+		}
+		virtual void comment(const char* c, const char* end)
+		{
+			s << "cmt{" << '#' << std::string(c, end) << "}";
+		}
+		virtual void begin_command(const char* c, const char* end)
+		{
+			s << "cmd{ident{" << std::string(c, end) << "}";
+		}
+		virtual void open_bracket()
+		{
+			s << '(';
+		}
+		virtual void close_bracket()
+		{
+			s << ')';
+		}
+		virtual void argument(const char* c, const char* end, bool quoted)
+		{
+			s << "arg{";
+			if(quoted)
+				s << '"' << std::string(c, end) << '"';
+			else
+				s << std::string(c, end);
+			s << "}";
+		}
+		virtual void end_command()
+		{
+			s << "}";
+		}
+
+		virtual ~listq() = default;
+	};
+
+	std::ifstream ifs;
+	std::istream* is = &std::cin;
+
+	if(argc > 1)
+	{
+		std::cout << "------ " << argv[1] << " ------\n";
+		ifs.open(argv[1], std::ios::in | std::ios::binary);
+		if(!ifs)
+			return 1;
+		is = &ifs;
+	}
+
+	std::noskipws(*is);
+	std::string source{std::istream_iterator<char>(*is), std::istream_iterator<char>()};
+
+	listp lst;
+	try
+	{
+		auto c = source.c_str();
+		auto end = c + source.size();
+
+		lst.parse(c, end);
+
+		std::string str = lst.s.str();
+		std::cout << str;
+		if(argc > 1)
+		{
+			if(diff(str, argv[1]))
+			{
+				std::cout << ">>>DIFF\n";
+				std::ofstream out("out.cmake");
+				out << str;
+			}
+			else
+			{
+				std::cout << ">>>SUCCESS\n";
+			}
+		}
+		else
+		{
+			std::cout << ">>>SUCCESS\n";
+		}
+		return 0;
+	}
+	catch(const cmake2::parser_error& ex)
+	{
+		std::cout << ex.what << '\n';
+		std::cout << lst.s.str();
+		std::cout << ">>>EXCEPTION\n";
+		throw;
+	}
+	return 0;
+}
 
--- cmake_editor/listparser.cpp
+++ cmake_editor/listparser.cpp
@@ -1,13 +1,375 @@
 /*
- * listparser.cpp
+ * listparser_t.cpp
  *
  *  Created on: 15/03/2013
  *      Author: nicholas
  */
 
 #include "listparser.h"
+#include <cctype>
 
 namespace cmake2
 {
+
+namespace
+{
+bool ws_p(const char c)
+{
+	switch(c)
+	{
+		case '\t':
+		case '\n':
+		case '\r':
+		case ' ':
+			return true;
+		default:
+			return false;
+	}
+}
+
+bool var_p(const char c)
+{
+	return isalnum(c) || c == '_';
+}
+
+bool argq_p(const char c)
+{
+	switch(c)
+	{
+		case '\n':
+		case '\r':
+		case '"':
+		case '#':
+		case '(':
+		case ')':
+		case '\\':
+			return false;
+		default:
+			return true;
+	}
+}
+
+bool arg_p(const char c)
+{
+	switch(c)
+	{
+		case '\t':
+		case '\n':
+		case '\r':
+		case ' ':
+		case '"':
+		case '#':
+		case '(':
+		case ')':
+		case '\\':
+			return false;
+		default:
+			return true;
+	}
+}
+
+bool ident_p(char c)
+{
+	return isalnum(c) || c == '_';
+}
+
+bool match_escape_character(const char*& c, const char* end)
+{
+	const auto begin = c;
+	if(*c != '\\')
+		return false;
+	++c;
+	if(c == end)
+	{
+		c = begin;
+		return false;
+	}
+
+	switch(*c)
+	{
+		case ' ':
+		case '"':
+		case '#':
+		case '$':
+		case '(':
+		case ')':
+		case '0':
+		case ';':
+		case '@':
+		case '\\':
+		case '^':
+		case 'n':
+		case 'r':
+		case 't':
+			break;
+		default:
+		{
+			c = begin;
+			return false;
+		}
+	}
+
+	++c;
+	return true;
+}
+
+bool match_variable(const char*& c, const char* end)
+{
+	const auto begin = c;
+	if(*c != '$')
+		return false;
+	++c;
+
+	if(c == end)
+	{
+		c = begin;
+		return false;
+	}
+
+	if(*c != '(')
+	{
+		c = begin;
+		return false;
+	}
+	++c;
+
+	if(c == end)
+	{
+		c = begin;
+		return false;
+	}
+
+	while(c != end && var_p(*c))
+		++c;
+
+	if(c == end)
+	{
+		c = begin;
+		return false;
+	}
+	if(*c != ')')
+	{
+		c = begin;
+		return false;
+	}
+	++c;
+
+	return true;
+}
+
+bool match_quoted(const char*& c, const char* end)
+{
+	const auto begin = c;
+	if(*c != '"')
+		return false;
+	++c;
+
+	while(c != end)
+	{
+		if(match_variable(c, end))
+		{
+		}
+		else if(argq_p(*c))
+		{
+			++c;
+		}
+		else if(match_escape_character(c, end))
+		{
+		}
+		else if(*c == '"')
+		{
+			++c;
+			return true;
+		}
+		else
+		{
+			c = begin;
+			return false;
+		}
+	}
+
+	c = begin;
+	return false;
+}
+
+bool match_arg(const char*& c, const char* end)
+{
+	if(!arg_p(*c))
+		return false;
+	++c;
+
+	while(c != end && arg_p(*c))
+		++c;
+	return true;
+}
+
+bool match_identifier(const char*& c, const char* end)
+{
+	if(!(isalpha(*c) || *c == '_'))
+		return false;
+	++c;
+
+	while(c != end && ident_p(*c))
+		++c;
+	return true;
+}
+
+}
+
+bool listparser_t::parse_whitespace(const char*& c, const char* end)
+{
+	if(!ws_p(*c))
+		return false;
+
+	const auto begin = c;
+	while(c != end && ws_p(*c))
+		++c;
+	whitespace(begin, c);
+	return true;
+}
+
+bool listparser_t::parse_comment(const char*& c, const char* end)
+{
+	if(*c != '#')
+		return false;
+
+	const auto begin = ++c;
+	while(c != end && *c != '\n')
+		++c;
+	comment(begin, c);
+	return true;
+}
+
+bool listparser_t::parse_quoted_argument(const char*& c, const char* end)
+{
+	if(*c != '"')
+		return false;
+
+	++c;
+	const auto begin = c;
+	while(c != end)
+	{
+		if(*c == '"')
+			break;
+		else if(match_escape_character(c, end))
+			continue;
+		++c;
+	}
+
+	if(c == end)
+		throw parser_error("quoted: unexpected eof; expected '\"'");
+	if(*c != '"')
+		throw parser_error("quoted: expected '\"'");
+
+	argument(begin, c, true);
+	++c;
+	return true;
+}
+
+bool listparser_t::parse_unquoted_argument(const char*& c, const char* end)
+{
+	const auto begin = c;
+	if(match_identifier(c, end))
+	{
+		argument(begin, c, false);
+		return true;
+	}
+
+	if(match_variable(c, end) || match_arg(c, end) || match_escape_character(c, end))
+	{
+	}
+	else
+	{
+		return false;
+	}
+
+	while(c != end)
+	{
+		if(match_variable(c, end) || match_arg(c, end) || match_escape_character(c, end) || match_quoted(c, end))
+		{
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	argument(begin, c, false);
+	return true;
+}
+
+bool listparser_t::parse_command(const char*& c, const char* end)
+{
+	const auto begin = c;
+	if(!match_identifier(c, end))
+		return false;
+
+	begin_command(begin, c);
+
+	while(c != end)
+	{
+		if(parse_whitespace(c, end) || parse_comment(c, end))
+			continue;
+		else if(*c == '(')
+			break;
+		else
+			throw parser_error("command: unexpected char; expected ws / cmt / (");
+	}
+
+	if(c == end)
+		throw parser_error("command: unexpected eof");
+
+	if(*c != '(')
+		throw parser_error("command: expected '('");
+
+	open_bracket();
+	++c;
+	std::size_t nesting(0);
+
+	while(c != end)
+	{
+		if(parse_whitespace(c, end) || parse_comment(c, end) || parse_quoted_argument(c, end) || parse_unquoted_argument(c, end))
+			continue;
+		else if(*c == '(')
+		{
+			open_bracket();
+			++c;
+			++nesting;
+		}
+		else if(*c == ')')
+		{
+			close_bracket();
+			++c;
+			if(nesting == 0)
+			{
+				end_command();
+				return true;
+			}
+			else
+			{
+				--nesting;
+			}
+		}
+		else
+			throw parser_error("command: unexpected char");
+	}
+
+	throw parser_error("command: unexpected eof");
+}
+
+void listparser_t::parse(const char* c, const char* end)
+{
+	while(c != end)
+	{
+		if(parse_whitespace(c, end) || parse_comment(c, end) || parse_command(c, end))
+			continue;
+		else
+			throw parser_error("parse: expected wsp / comment / command");
+	}
+}
+
+listparser_t::~listparser_t()
+{
+}
 
 } /* namespace cmake2 */
