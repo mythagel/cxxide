c881b6dd46b2db934c3ddcbf6610b7f9982f920f
mythagel <mythagel@gmail.com>
2013-09-27 15:34:43 +1000

    git experiments

--- ex/git_basic.cpp
+++ ex/git_basic.cpp
@@ -1,7 +1,8 @@
 #include "git2.h"
 #include <stdio.h>
+#include <cstring>
 
-static void fetch_progress(const git_transfer_progress *stats, void *payload)
+static int fetch_progress(const git_transfer_progress *stats, void *payload)
 {
     int fetch_percent = (100 * stats->received_objects) / stats->total_objects;
     int index_percent = (100 * stats->indexed_objects) / stats->total_objects;
@@ -12,6 +13,33 @@ static void fetch_progress(const git_transfer_progress *stats, void *payload)
             stats->received_objects, stats->total_objects,
             index_percent,
             stats->indexed_objects, stats->total_objects);
+    return 0;
+}
+
+static void show_branch(git_repository *repo)
+{
+  int error = 0;
+  const char *branch = NULL;
+  git_reference *head = NULL;
+
+  error = git_repository_head(&head, repo);
+
+  if (error == GIT_EUNBORNBRANCH || error == GIT_ENOTFOUND)
+    branch = NULL;
+  else if (!error) {
+    branch = git_reference_name(head);
+    if (!strncmp(branch, "refs/heads/", strlen("refs/heads/")))
+      branch += strlen("refs/heads/");
+  }
+  else
+  {
+        const git_error *error = giterr_last();
+        printf("Error %d %s - %s\n", error, "show_branch", (error && error->message) ? error->message : "???");
+  }
+
+    printf("# On branch %s\n", branch ? branch : "Not currently on any branch.");
+
+  git_reference_free(head);
 }
 
 static int do_clone(const char *url, const char *path)
@@ -22,9 +50,43 @@ static int do_clone(const char *url, const char *path)
 
     opts.fetch_progress_cb = fetch_progress;
     ret = git_clone(&repo, url, path, &opts);
+    
+    if(ret == GIT_ERROR)
+    {
+        const git_error *error = giterr_last();
+        printf("Error %d %s - %s\n", ret, "clone", (error && error->message) ? error->message : "???");
+    }
+    
+    show_branch(repo);
     git_repository_free(repo);
+    
     return ret;
 }
+
+/*
+ load a repo
+git_repository *repo;
+int error = git_repository_open(
+  &repo,
+  "/path/to/repository/on/disk");
+
+init new
+git_repository *repo;
+int error = git_repository_init(
+  &repo,                // output
+  "path/to/new/repo",   // path
+  false);               // bare?
+
+Clone and get branch name above.
+Still Need
+    branch
+    merge
+    commit
+    push
+    pull
+
+*/
+
 int main(int argc, char **argv)
 {
     const char *url, *path;
