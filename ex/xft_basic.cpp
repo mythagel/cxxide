adb059ae50d2f05d9516812278d7eb5c2d55644e
Nicholas Gill <mythagel@gmail.com>
2013-10-01 20:48:35 +1000

    multiline text

--- ex/xft_basic.cpp
+++ ex/xft_basic.cpp
@@ -5,89 +5,49 @@
 #include <X11/Xutil.h>
 #include <Xft/Xft.h>
 #include <string>
+#include <vector>
 
 #define DEFAULT_BDWIDTH 1 /* border width */
-
-Display *dpy;
 
 int main( int argc, char *argv[] )
 {
    int                     ytext, wtext, htext, gravity, geo_mask;
    int                     Done = 0, highlight = 0, force_expose = 0;
-   XSizeHints              xsh;
-   XWMHints                xwmh;
-   unsigned long           fgpix, bgpix;
    Window                  mainW;
    XSetWindowAttributes    xswa;
-   XftFont                 *font;
    XftDraw                 *xftdraw;
    XRenderColor            xrcolor;
    XftColor                xftcolor, hlcolor;
-   XGlyphInfo              extents;
    XEvent                  event;
    char                    user_geo[80];
  
-   std::string text;
-   text = "int main()\n\
-{\n\
-    int main_local;\n\
-    \n\
-    test t;\n\
-    t.member = 5;\n\
-    for(int i = 0; i < 10; ++i)\n\
-        main_local += t.mem_func() + i;\n\
-    \n\
-    return main_local;\n\
-}";
+    std::vector<std::string> text;
+    text.push_back("int main()");
+    text.push_back("{");
+    text.push_back("    int main_local;");
+    text.push_back("    ");
+    text.push_back("    test t;");
+    text.push_back("    t.member = 5;");
+    text.push_back("    for(int i = 0; i < 10; ++i)");
+    text.push_back("        main_local += t.mem_func() + i;");
+    text.push_back("    ");
+    text.push_back("    return main_local;");
+    text.push_back("}");
    
    /* open connection to X display */
-   dpy = XOpenDisplay( NULL );
+   auto dpy = XOpenDisplay( NULL );
    
    /* set up font */
-   font = XftFontOpenName( dpy, DefaultScreen( dpy ), "Source Code Pro-9" ); 
+   auto font = XftFontOpenName( dpy, DefaultScreen( dpy ), "Source Code Pro-12" ); 
    
    /* colors */
-   fgpix = BlackPixel( dpy, DefaultScreen( dpy ) );
-   bgpix = WhitePixel( dpy, DefaultScreen( dpy ) );
-   
-   /* position and size of top window (XSizeHints) */
-   XftTextExtents8( dpy, font, (const FcChar8*)text.c_str(), text.size(), &extents );
-   ytext = extents.height - extents.y;
-   wtext = extents.width - extents.x;
-   htext = ytext + 4;   
-   xsh.flags = ( PPosition | PSize | PMinSize );
-   xsh.height = htext + 10;
-   xsh.min_height = xsh.height;
-   xsh.width = wtext;
-   xsh.min_width = xsh.width;
-   xsh.x = 50;
-   xsh.y = 50;
-   
-   /* construct a geometry string */
-   sprintf( user_geo, "%dx%d+%d+%d", xsh.width, xsh.height, xsh.x, xsh.y );
-   
-   /* process geometry specification */
-   geo_mask = XWMGeometry( dpy, DefaultScreen(dpy), user_geo, NULL/*def_geo*/,
-                  DEFAULT_BDWIDTH, &xsh, &xsh.x, &xsh.y, &xsh.width, &xsh.height,
-                  &gravity );
-   
-   /* check geometry bitmask and set size hints */
-   if ( geo_mask & (XValue|YValue) ) xsh.flags |= USPosition;
-   if ( geo_mask & (WidthValue|HeightValue) ) xsh.flags |= USSize;
+   auto fgpix = BlackPixel( dpy, DefaultScreen( dpy ) );
+   auto bgpix = WhitePixel( dpy, DefaultScreen( dpy ) );
    
    /* create top level window */
-   mainW = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), xsh.x, xsh.y, 
-               xsh.width, xsh.height, DEFAULT_BDWIDTH, fgpix, bgpix );
+   mainW = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 
+               800, 600, DEFAULT_BDWIDTH, fgpix, bgpix );
    
-   /* set window manager properties */
-   XSetStandardProperties( dpy, mainW, "xhello", "xhello", None, argv, argc, &xsh );
-   
-   /* set window manager hints */
-   xwmh.flags = (InputHint|StateHint);
-   xwmh.input = False;
-   xwmh.initial_state = NormalState;
-   XSetWMHints( dpy, mainW, &xwmh );
-
    /* Xft draw context */
    xftdraw = XftDrawCreate( dpy, mainW, DefaultVisual(dpy,DefaultScreen(dpy)),
                DefaultColormap( dpy, DefaultScreen(dpy) ) );
@@ -116,7 +76,6 @@ int main( int argc, char *argv[] )
    
    /* make window visible */
    XMapWindow( dpy, mainW );
-   printf("click on the window to exit\n");
    
    /* retrieve and process events */
    while ( !Done ) {
@@ -130,17 +89,22 @@ int main( int argc, char *argv[] )
                force_expose = 1;
             case Expose: 
                if ( event.xexpose.count == 0 || force_expose ) {
-                  int            x, y, itmp;
+                  int             itmp;
                   unsigned int   w, h, utmp;
                   Window         wtmp;
 
                   if ( XGetGeometry( dpy, mainW, &wtmp, &itmp, &itmp, &w, &h, &utmp, &utmp )==0 )
                      break;
                   XClearWindow( dpy, mainW );
-                  x = ( w - extents.width ) / 2;
-                  y = htext + ( h - htext + extents.height )/2;
-                  if (highlight) XftDrawRect( xftdraw, &hlcolor, x, y-extents.height, wtext, extents.height  );
-                  XftDrawString8( xftdraw, &xftcolor, font, x, y, (const FcChar8*)text.c_str(), text.size() );
+                  
+                  int y = 20;
+                  for(auto& line : text)
+                  {
+                      XGlyphInfo extents;
+                      XftTextExtentsUtf8 (dpy, font, (const FcChar8*)line.c_str(), line.size(), &extents);
+                      XftDrawStringUtf8( xftdraw, &xftcolor, font, 10, y, (const FcChar8*)line.c_str(), line.size() );
+                      y = y + 4 + extents.height;
+                  }
                   force_expose = 0;
                }
                break;
