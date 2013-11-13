eedf2b7380d4e5264bdd17d7f05a75bc0ab562ea
mythagel <mythagel@gmail.com>
2013-11-13 15:42:54 +1100

    cli implementation

--- src/microide.cpp
+++ src/microide.cpp
@@ -49,9 +49,10 @@ source create tu blah [--lang c++/c]
 
 */
 
-typedef void command_t(int argc, char* argv[]);
+typedef int command_t(int argc, char* argv[]);
 
-void create(int argc, char* argv[]);
+int create(int argc, char* argv[]);
+int dir(int argc, char* argv[]);
 
 po::options_description common_options("Global options");
 po::options_description user_options("microide options");
@@ -69,14 +70,14 @@ int main(int argc, char* argv[])
         po::options_description hidden;
         hidden.add_options()
             ("command", po::value<std::string>()->required(), "Command to execute")
-            ("subcommand_args", po::value<std::string>(), "Subcommand arguments")
+            ("command_args", po::value<std::vector<std::string>>(), "Command arguments")
         ;
 
         user_options.add(common_options);
 
         po::positional_options_description p;
         p.add("command", 1);
-        p.add("subcommand_args", -1);
+        p.add("command_args", -1);
         po::options_description desc;
         desc.add(common_options).add(hidden);
 
@@ -100,14 +101,14 @@ int main(int argc, char* argv[])
 
         std::map<std::string, command_t*> commands =
         {
-            {"create", create}
+            {"create", create},
+            {"dir", dir}
         };
 
         auto command = commands.find(cmd);
         if(command != commands.end())
         {
-            command->second(argc, argv);
-            return 0;
+            return command->second(argc, argv);
         }
         else
         {
@@ -129,7 +130,7 @@ int main(int argc, char* argv[])
     }
 }
 
-void create(int argc, char* argv[])
+int create(int argc, char* argv[])
 {
     po::options_description command_options("Create options");
     command_options.add_options()
@@ -152,24 +153,86 @@ void create(int argc, char* argv[])
     po::variables_map vm;
     store(po::command_line_parser(argc, argv). positional(p).options(desc).run(), vm);
 
-    if (vm.count("help"))
+    if(vm.count("help"))
     {
         std::cout << user_options << "\n";
-        return;
+        return 0;
     }
     notify(vm);
 
-    auto path = vm["path"].as<std::string>();
+    fs::path path = vm["path"].as<std::string>();
     auto name = vm["name"].as<std::string>();
-    
+    fs::path build_path;
     if(vm.count("build-path"))
+        build_path = vm["build-path"].as<std::string>();
+
+    auto project = project::create(name, path, build_path);
+
+    return 0;
+}
+
+int dir(int argc, char* argv[])
+{
+    po::options_description command_options("Directory options");
+    command_options.add_options()
+//        ("name", po::value<std::string>()->required(), "Directory name")
+    ;
+
+    po::options_description hidden;
+    hidden.add_options()
+        ("command", po::value<std::string>()->required(), "Command to execute")
+        ("subcommand", po::value<std::string>()->required(), "Subcommand to execute")
+        ("subcommand_args", po::value<std::vector<std::string>>(), "Subcommand arguments")
+    ;
+
+    user_options.add(command_options);
+
+    po::positional_options_description p;
+    p.add("command", 1);
+    p.add("subcommand", 1);
+    p.add("subcommand_args", -1);
+    po::options_description desc;
+    desc.add(common_options).add(command_options).add(hidden);
+
+    po::variables_map vm;
+    store(po::command_line_parser(argc, argv). positional(p).options(desc).run(), vm);
+
+    if (vm.count("help"))
     {
-        auto build_path = vm["build-path"].as<std::string>();
-        auto project = project::create(name, path, build_path);
+        std::cout << user_options << "\n";
+        return 0;
+    }
+    notify(vm);
+
+    /* Better idea would be to part the initial generic directory arguments
+     * Then reparse based on the selected subcommand. */
+
+    fs::path path = vm["path"].as<std::string>();
+    fs::path build_path;
+    if(vm.count("build-path"))
+        build_path = vm["build-path"].as<std::string>();
+
+    auto cmd = vm["subcommand"].as<std::string>();
+    auto args = vm["subcommand_args"].as<std::vector<std::string>>();
+
+    auto project = project::open(path, build_path);
+
+    if(cmd == "create")
+    {
+        if(args.size() != 1)
+        {
+            std::cout << user_options << "\n";
+            return 1;
+        }
+        // TODO
     }
     else
     {
-        auto project = project::create(name, path);
+        std::cerr << "Unrecognised subcommand " << cmd << "\n";
+        std::cout << user_options << "\n";
+        return 1;
     }
+
+    return 0;
 }
 
